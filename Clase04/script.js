0099df5375351be3e7c6097ba0e09c6b3a1589a3
Lucas-rd <lucasrdoval@gmail.com>
2022-03-05 20:19:41 -0300

    commit clase 04

---
+++ Clase04/script.js
@@ -0,0 +1,33 @@
+const {fromEvent, Observable} = rxjs; //fromEvent es un creador de observables de eventos del DOM
+const { switchMap, tap, map } = rxjs.operators;
+
+const input = document.getElementById("miInput");
+const span = document.getElementById("miParrafo");
+
+//Observable
+const inputObservavble = fromEvent(input, 'input').pipe(
+    map(()=> input.value),
+    switchMap(texto => new Observable(observer =>{
+        if(texto === "error"){
+            observer.error("El usuario ingreso ERROR")
+        } else if(texto === "complete"){
+            observer.complete(console.log("El usuario ingreso COMPLETE"))
+        } else {
+            observer.next(texto)
+        }
+    }))
+);
+
+//observer
+const subscripcion = inputObservavble.subscribe({
+    next: (val) => span.innerHTML = val.split("").reverse().join(""),
+    error: (error) => console.error(error),
+    complete: (complete) =>console.log(complete)
+});
+
+setTimeout(()=>{
+    subscripcion.unsubscribe();
+    span.innerHTML = "";
+    input.value = "";
+}, 30000);
+
